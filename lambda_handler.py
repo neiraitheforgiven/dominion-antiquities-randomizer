21550e9b2e93aea6e5bcf5c7b0405af701fda61f
Curtis den Otter <curtis.denotter@gmail.com>
2019-09-27 21:24:02 -0600

    Get set toggling working via the web page

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,10 +3,22 @@ from randomizer import RandomizeDominion
 
 
 def lambda_handler(event, context):
-    return {
+    print("Received event: " + json.dumps(event, indent=2))
+
+    response = {
         'statusCode': 200,
         'headers': {
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps(RandomizeDominion().splitlines())
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        }
     }
+
+    if event['requestContext']['httpMethod'] == 'POST':
+        body = json.loads(event['body'] or '')
+        if 'sets' in body:
+            data = RandomizeDominion(body['sets'])
+        else:
+            data = RandomizeDominion()
+        response['body'] = json.dumps(data)
+
+    return response
--- website/scripts/randomizer.js
+++ website/scripts/randomizer.js
@@ -1,14 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    const button = document.getElementById('randomize');
+    const form = document.forms[0];
     const cards = document.getElementById('cards');
     const url = 'https://nv1gwscvf9.execute-api.us-west-2.amazonaws.com/default/DominionRandomizer/'
 
-    button.addEventListener('click', (event) => {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
         if (cards.children.length > 0) {
             cards.children[0].remove()
         }
 
-        fetch(url).then((response) => response.json()).then((data) => {
+        var data = {
+            sets: []
+        };
+
+        for (var i = 0; i < form.sets.elements.length; i++) {
+            let checkbox = form.sets.elements[i];
+
+            if (checkbox.checked) {
+                data.sets.push(checkbox.value);
+            }
+        }
+
+        fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => response.json()).then((data) => {
             let ul = document.createElement('ul');
             for (var i = 0; i < data.length; i++) {
                 let li = document.createElement('li');
