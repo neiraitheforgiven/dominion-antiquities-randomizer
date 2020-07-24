6bd454a3509085c1f5ae86139752fd7d705a4cc5
Curtis den Otter <curtis.denotter@computronix.com>
2020-07-24 00:48:48 -0600

    Autoformat missed files using Prettier and Black

--- lambda_handler.py
+++ lambda_handler.py
@@ -6,19 +6,19 @@ def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
     response = {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+        },
     }
 
-    if event['requestContext']['httpMethod'] == 'POST':
-        body = json.loads(event['body'] or '{}')
-        sets = body.get('sets') or None
-        options = body.get('options')
+    if event["requestContext"]["httpMethod"] == "POST":
+        body = json.loads(event["body"] or "{}")
+        sets = body.get("sets") or None
+        options = body.get("options")
 
         data = RandomizeDominion(sets, options)
-        response['body'] = json.dumps(data)
+        response["body"] = json.dumps(data)
 
     return response
