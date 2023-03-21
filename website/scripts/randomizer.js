c54b5017eebba2169f81c2aa0284abe636a3d34e
Curtis den Otter <curtis.denotter@gmail.com>
2023-03-20 22:37:22 -0600

    Handle equipment in web view

--- website/scripts/randomizer.js
+++ website/scripts/randomizer.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     const form = document.forms[0];
     const cards = document.getElementById("cards");
+    const handleExtras = (i, cardData, extraElements) => {
+        let h3 = document.createElement("h3");
+        let headerText = document.createTextNode(cardData[i]);
+        h3.appendChild(headerText);
+        i++;
+
+        let ul = document.createElement("ul");
+        for (var j = i; j < cardData.length; j++) {
+            let li = document.createElement("li");
+            let text = document.createTextNode(cardData[j]);
+            li.appendChild(text);
+            ul.appendChild(li);
+        }
+
+        extraElements.push(h3);
+        extraElements.push(ul);
+    };
 
     form.addEventListener(
         "submit",
@@ -9,7 +26,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
             const pyodide = globalThis.pyodide;
 
             if (cards.children.length > 0) {
-                cards.children[0].remove();
+                for (var i = cards.children.length - 1; i >= 0; i--) {
+                    cards.children[i].remove();
+                }
             }
 
             var data = {
@@ -44,15 +63,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
             `);
             let cardData = proxy.toJs();
             let ul = document.createElement("ul");
+            let extraElements = [];
             for (var i = 0; i < cardData.length; i++) {
+                if (cardData[i] == "Extras:") {
+                    handleExtras(i, cardData, extraElements);
+                    break;
+                }
+
                 let li = document.createElement("li");
                 let text = document.createTextNode(cardData[i]);
                 li.appendChild(text);
                 ul.appendChild(li);
             }
             cards.appendChild(ul);
+            for (let element of extraElements) {
+                cards.appendChild(element);
+            }
+
             cards.scrollIntoView({ behavior: "smooth" });
-            proxy.destroy()
+            proxy.destroy();
         },
         false
     );
